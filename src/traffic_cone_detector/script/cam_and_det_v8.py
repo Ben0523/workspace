73342dda3cb49199f9140739a0808734b6a8a1da
Torok <beni.torok23@gmail.com>
2025-03-02 14:53:14 +0100

    implementation of pygame

--- src/traffic_cone_detector/script/cam_and_det_v8.py
+++ src/traffic_cone_detector/script/cam_and_det_v8.py
@@ -5,6 +5,7 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ultralytics import YOLO
+import pygame
 
 class YoloConeDetector:
     def __init__(self, camera_port=0):
@@ -16,6 +17,18 @@ class YoloConeDetector:
         self.imgsz = rospy.get_param("~image_size", 480)
         rospy.loginfo("Model loaded successfully!")
         rospy.loginfo(f"Total model parameters: {sum(p.numel() for p in self.model.model.parameters())}")
+
+        #pygame
+        pygame.init()
+        self.colors = {}
+        self.running = True
+        self.screen_width, self.screen_height = 640, 480
+        self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
+        pygame.display.set_caption("Cone locations")  
+        self.scale = 1
+        self.offset_x, self.offset_y = 0,0
+        self.robot_triangle_coordinates = [(self.screen_width//2-10, self.screen_height),(self.screen_width//2+10, self.screen_height),(self.screen_width//2, self.screen_height-20)]
+        #----
         
         self.cap = cv2.VideoCapture(camera_port)
         self.rate = rospy.Rate(30)
@@ -46,8 +59,37 @@ class YoloConeDetector:
             if self.det_image_pub.get_num_connections():
                 self.det_image_pub.publish(self.bridge.cv2_to_imgmsg(det_annotated, encoding='bgr8'))
                 rospy.loginfo("Publishing detection result...")
+
+            self.render(center_points)
             
             self.rate.sleep()
+
+    def render(self, dataset):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        self.screen.fill((255,255,255))
+        font = pygame.font.Font(None, 32)
+        #pygame.draw.polygon(self.screen, [255,0,0], self.robot_triangle_coordinates)
+        #robot_label = font.render("robot", [0,0,0], True)
+        #self.screen.blit(robot_label,(int(self.offset_x), int(self.offset_y - 25)))
+
+
+
+        for point in dataset:
+            x, y = point[0], point[1]
+            screen_x, screen_y = self.transform_point(x, y)
+            pygame.draw.circle(self.screen,[0,0,0], (screen_x, screen_y), 5)
+            
+        pygame.display.flip()   
+
+    def transform_point(self,x, y):
+        screen_x = int(x * self.scale + self.offset_x)
+        screen_y = int(y * self.scale + self.offset_y)
+        return screen_x, screen_y
 
 if __name__ == "__main__":
     try:
